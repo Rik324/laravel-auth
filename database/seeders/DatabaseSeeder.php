2e1484ff608a34ca1ac353b003323640f110bf4c
Rik324 <rishabhkool786@gmail.com>
2025-07-25 16:01:07 +0700

    6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,15 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // This line tells Laravel to run your ProductSeeder
         $this->call([
             ProductSeeder::class,
-            DojoSeeder::class,
-            NinjaSeeder::class,
-        ]);
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
         ]);
     }
 }
